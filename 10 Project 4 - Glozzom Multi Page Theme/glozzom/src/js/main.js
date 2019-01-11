5c9596f818ea48bd11ab3923a4f4eb18f06e67e6
musichopin <merepianist@hotmail.com>
2019-01-11 09:57:12 +0300

    reviewed

--- 10 Project 4 - Glozzom Multi Page Theme/glozzom/src/js/main.js
+++ 10 Project 4 - Glozzom Multi Page Theme/glozzom/src/js/main.js
@@ -1,13 +1,15 @@
 $(function() {//jQuery shorthand 4 $(document).ready(function() { ... });
   // Auto play modal video (on index.html)
   $(".video").click(function () {
-    var theModal = $(this).data("target"), /*alt: $(this).attr("data-target")*/
-    videoSRC = $(this).attr("data-video"),/*gets data-video attr value,ie link*/
+    var theModal = $(this).data("target"),//alt:$(this).attr("data-target")
+    videoSRC = $(this).attr("data-video"),/*gets the link*/
     videoSRCauto = videoSRC + 
       "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
     $(theModal + ' iframe').attr('src', videoSRCauto); /*sets the src*/
     $(theModal + ' button.close').click(function () { /*4 closing yt frame*/
-      $(theModal + ' iframe').attr('src', videoSRC);
+      $(theModal + ' iframe').attr('src', videoSRC); // disrupts video
+//videoSRC yerine başka isim de kullanılabilirdi
+/*modal oldugu icin otomatik kapanmanın js ile ilgisi yoktur*/   
     });
   });
 });
